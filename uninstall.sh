35c031659050b7ebfaccae2fdfd56192f5b6827a
shockage1 <103493161+shockage1@users.noreply.github.com>
2022-04-27 21:13:43 -0400

    Add files via upload

--- uninstall.sh
+++ uninstall.sh
@@ -8,12 +8,18 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-systemctl disable doorbell
-systemctl stop doorbell
-rm /usr/local/bin/doorbell.py
-rm /etc/systemd/system/doorbell.service
-rm /var/log/doorbell.log # Make backup if you want to keep logs, journalctl -u doorbell.service still keeps a copy though
-sed -i '\;/var/log/doorbell;d' /etc/logrotate.d/rsyslog
-sed -i '\;local5.*;d' /etc/rsyslog.conf
-systemctl daemon-reload
-systemctl reset-failed
+# Abort uninstallation if python script is not already in /usr/local/bin
+if [[ -f "/usr/local/bin/doorbell.py" ]]; then
+	systemctl disable doorbell
+	systemctl stop doorbell
+	rm /usr/local/bin/doorbell.py
+	rm /etc/systemd/system/doorbell.service
+	rm /var/log/doorbell.log # Make backup if you want to keep logs, journalctl -u doorbell.service still keeps a copy though
+	sed -i '\;/var/log/doorbell;d' /etc/logrotate.d/rsyslog
+	sed -i '\;local5.*;d' /etc/rsyslog.conf
+	systemctl daemon-reload
+	systemctl reset-failed
+else
+	echo "Doorbell system not already installed"
+	exit 0
+fi
